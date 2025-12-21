0780f381ed2379bf39b6fb529e28ac367e806a76
zawarali0302-commits <zawarali0302@gmail.com>
2025-12-21 15:09:18 +0500

    ..

--- frontend/src/components/BannerComponent.tsx
+++ frontend/src/components/BannerComponent.tsx
@@ -1,7 +1,7 @@
 
 const BannerComponent = () => {
   return (
-    <div className="flex flex-col items-center justify-center bg-blue-500  text-white p-4 mt-4 m-10 rounded-2xl">
+    <div className={`flex flex-col items-center justify-center bg-linear-to-r from-blue-500 to-blue-900 h-72  text-white p-4 mt-4 m-10 rounded-2xl`}>
       <h1 className="text-3xl">Welcome to the Quotes Management App</h1>
       <p className="mt-2">Your one-stop solution for managing quotes</p>
     </div>
--- frontend/src/components/HeaderComponent.tsx
+++ frontend/src/components/HeaderComponent.tsx
@@ -1,13 +1,12 @@
 
 const HeaderComponent = () => {
   return (
-    <div className="flex justify-between bg-blue-500 text-white p-4">
-      <h1 className="text-2xl">Quotes Management App</h1>
+    <div className="flex justify-between bg-linear-to-r from-blue-500 to-blue-900 text-white p-4">
+      <h1 className="text-2xl font-bold">Quotes Management App</h1>
 
       <ul className="flex items-center gap-6 mr-8">
-        {['Home', 'Top Collection', 'About'].map((item) => (
-            <li><a href="#">{item}</a></li>
-        ))}
+        <li><a href="">Home</a></li>
+        <li className="bg-linear-to-tl from-blue-500 to-blue-900 hover:bg-linear-to-bl cursor-pointer px-4 py-2 rounded" >Add Quote</li>
       </ul>
     </div>
   )
