4ce289050492bd5616b3b123c0f6bce003adea64
zawarali0302-commits <zawarali0302@gmail.com>
2025-12-31 18:07:52 +0500

    Update HeaderComponent.tsx

--- frontend/src/components/HeaderComponent.tsx
+++ frontend/src/components/HeaderComponent.tsx
@@ -3,7 +3,7 @@ interface HeaderComponentProps{
 }
 const HeaderComponent = ({handleOpenForm}: HeaderComponentProps) => {
   return (
-    <div className="flex justify-between bg-linear-to-r  from-blue-500 to-blue-900 text-white p-4 sticky z-10">
+    <div className="flex justify-between bg-linear-to-r  from-blue-500 to-blue-900 text-white p-4 sticky top-0 z-10">
       <h1 className="text-2xl font-bold">Quotes Management App</h1>
 
       <ul className="flex items-center gap-6 mr-8">
